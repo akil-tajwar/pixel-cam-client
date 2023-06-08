378b0a63eddfd0970675bc1c633eddba68c844e1
akil-tajwar <akiltajwar@gmail.com>
2023-06-09 00:13:56 +0600

    adding logout

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -1,7 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Providers/AuthProvider';
 
 const Navbar = () => {
+    const { user, logout } = useContext(AuthContext);
+    const handleLogout = () => {
+        logout()
+            .then(() => { })
+            .catch(error => console.log(error))
+    }
+
     const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
     useEffect(() => {
         localStorage.setItem('theme', theme);
@@ -9,7 +17,7 @@ const Navbar = () => {
         document.querySelector('html').setAttribute('data-theme', localTheme);
     }, [theme])
     const handleToggle = (e) => {
-        if(e.target.checked) {
+        if (e.target.checked) {
             setTheme('light');
         }
         else {
@@ -21,7 +29,7 @@ const Navbar = () => {
             <div className="fixed w-3/4 mx-auto z-10 text-white rounded-lg bg-[#171818]">
                 <div className='flex justify-between items-center p-3'>
                     <div className='flex gap-2 justify-center items-center'>
-                        <img className='w-14' src="/public/pixelcam.png" alt="" /> 
+                        <img className='w-14' src="/public/pixelcam.png" alt="" />
                         <div>
                             <h3 className="text-3xl text-[#00dcf4] font-semibold">P<span className='text-[#ff9523]'>i</span>xelCam Academy</h3>
                         </div>
@@ -30,8 +38,13 @@ const Navbar = () => {
                         <Link className='hover:text-[#D99904]' to={'/'}>Home</Link>
                         <Link className='hover:text-[#D99904]' to={'/instructors'}>Instructors</Link>
                         <Link className='hover:text-[#D99904]' to={'/classes'}>Classes</Link>
-                        <Link className='hover:text-[#D99904]' to={'/login'}>Login</Link>
-                        <Link className='hover:text-[#D99904]' to={'/signup'}>Signup</Link>
+                        {
+                            user ? <p onClick={handleLogout} className='hover:text-[#D99904] cursor-pointer'>Logout</p> : <Link className='hover:text-[#D99904]' to={'/login'}>Login</Link>
+                        }
+                        {
+                            user ? <div className='w-10 h-10 relative'><img className='rounded-full w-full h-full object-cover border-2 border-[#78bf4d]' src={user.photoURL} title={user.displayName} alt="user photo" /></div> : <Link className='hover:text-[#D99904]' to={'/signup'}>Signup</Link>
+                        }
+                        
                         <label className="swap swap-rotate">
                             <input type="checkbox" onChange={handleToggle} />
                             <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
