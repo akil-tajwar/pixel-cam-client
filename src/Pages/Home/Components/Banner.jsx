4dffe2d62571cb6ee6b4d6c612a5fb332b928ad5
akil-tajwar <akiltajwar@gmail.com>
2023-06-11 01:42:06 +0600

    changing navbar and banner style

--- src/Pages/Home/Components/Banner.jsx
+++ src/Pages/Home/Components/Banner.jsx
@@ -4,18 +4,38 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Banner = () => {
     return (
-        <Carousel className='w-3/4 mx-auto mt-16 text-center mb-20'>
-            <div>
-                <img className='rounded-lg' src="/public/banner2.jpg" />
+        <Carousel className='w-3/4 rounded-lg mx-auto mt-24 text-center mb-20'>
+            <div className='bg-[url(/public/banner2.jpg)] bg-cover'>
+                <div className='bg-black bg-opacity-80 grid grid-cols-3 gap-5 justify-center items-center text-white p-20'>
+                    <h1 className='text-7xl text-left'>Unleash Your Creative Vision</h1>
+                    <div className='col-span-2'>
+                        <img className='' src="/public/banner2.jpg" alt="" />
+                    </div>
+                </div>
             </div>
-            <div>
-                <img className='rounded-lg' src="/public/banner3.jpg" />
+            <div className='bg-[url(/public/banner3.jpg)] bg-cover'>
+                <div className='bg-black bg-opacity-80 grid grid-cols-3 gap-5 justify-center items-center text-white p-20'>
+                    <h1 className='text-7xl text-left'>Capture Moments, Tell Stories</h1>
+                    <div className='col-span-2'>
+                        <img className='' src="/public/banner3.jpg" alt="" />
+                    </div>
+                </div>
             </div>
-            <div>
-                <img className='rounded-lg' src="/public/banner4.jpg" />
+            <div className='bg-[url(/public/banner4.jpg)] bg-cover'>
+                <div className='bg-black bg-opacity-80 grid grid-cols-3 gap-5 justify-center items-center text-white p-20'>
+                    <h1 className='text-7xl text-left'>Discover <br /> the Art of Photography</h1>
+                    <div className='col-span-2'>
+                        <img className='' src="/public/banner4.jpg" alt="" />
+                    </div>
+                </div>
             </div>
-            <div>
-                <img className='rounded-lg' src="/public/banner1.jpg" />
+            <div className='bg-[url(/public/banner1.jpg)] bg-cover'>
+                <div className='bg-black bg-opacity-80 grid grid-cols-3 gap-5 justify-center items-center text-white p-20'>
+                    <h1 className='text-7xl text-left'>Unlock Your Potential</h1>
+                    <div className='col-span-2'>
+                        <img className='' src="/public/banner1.jpg" alt="" />
+                    </div>
+                </div>
             </div>
         </Carousel>
     );
--- src/Pages/Home/Components/PopularClasses.jsx
+++ src/Pages/Home/Components/PopularClasses.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../Shared/SectionTitle';
 
 const PopularClasses = () => {
+    const [classes, setClasses] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/classes')
+            .then(res => res.json())
+            .then(data => setClasses(data))
+            .catch(error => console.log(error))
+    }, [])
+    const popular = classes.filter(item => item.name === 'Popular');
     return (
-        <div>
+        <div className='w-3/4 mx-auto'>
             <SectionTitle title={'Popular Classes'}></SectionTitle>
+            <div className='grid grid-cols-3 gap-10 mt-4 mb-10 w-3/4 mx-auto'>
+                {
+                    popular.map(item => <div item={item} key={item._id}>
+                        <div className='w-full h-full relative'>
+                            <img className='w-full h-full object-cover' src={item.image} alt="" />
+                        </div>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
