91e05a67de874940c4664711e02f12b60130264e
akil-tajwar <akiltajwar@gmail.com>
2023-06-11 05:07:29 +0600

    adding instructors page

--- src/Pages/Instructors/Instructors.jsx
+++ src/Pages/Instructors/Instructors.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SectionTitle from '../Shared/SectionTitle';
 
 const Instructors = () => {
+    const [instructors, setInstructors] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/instructors')
+            .then(res => res.json())
+            .then(data => setInstructors(data))
+            .catch(error => console.log(error))
+    })
     return (
-        <div>
-            
+        <div className='lg:mt-24 mt-36'>
+            <SectionTitle title={'Meet Your Instructors'}></SectionTitle>
+            <div className='grid lg:grid-cols-3 grid-cols-1 gap-10 mt-4 mb-20 w-3/4 mx-auto'>
+                {
+                    instructors.map(item => <div item={item} key={item._id}>
+                        <div className='border-2 border-[#171818]'>
+                            <div className='w-full h-72 relative'>
+                                <img className='w-full h-full object-cover' src={item.image} alt="" />
+                            </div>
+                            <div className='p-3'>
+                                <h3 className='text-3xl font-semibold pb-2'>{item.name}</h3>
+                                <p>email: {item.email}</p>
+                            </div>
+                        </div>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
